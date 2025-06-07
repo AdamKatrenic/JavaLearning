9f85fa56ada0f515d911284cb9617b983d293468
AdamKatrenic <adamkatrenic@gmail.com>
2025-06-07 18:10:30 +0200

    Cvicenia

--- SetsAndMaps/src/dev/lpa/MapMain.java
+++ SetsAndMaps/src/dev/lpa/MapMain.java
@@ -38,9 +38,87 @@ public class MapMain {
         for (Contact contact : fullList) {
             Contact duplicate = contacts.put(contact.getName(), contact);
             if (duplicate != null) {
-                System.out.println("Duplicate = " + duplicate);
-                System.out.println("Current = " +  contact);
+//                System.out.println("Duplicate = " + duplicate);
+//                System.out.println("Current = " +  contact);
+                contacts.put(duplicate.getName(), contact.mergeContactData(duplicate));
             }
         }
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        contacts.clear();
+
+        for (Contact contact : fullList) {
+            contacts.putIfAbsent(contact.getName(), contact);
+        }
+
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        contacts.clear();
+
+        for (Contact contact : fullList) {
+            Contact duplicate = contacts.putIfAbsent(contact.getName(), contact);
+            if (duplicate != null) {
+                contacts.put(duplicate.getName(), contact.mergeContactData(duplicate));
+            }
+        }
+
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        contacts.clear();
+
+        fullList.forEach(contact -> contacts.merge(contact.getName(), contact,
+                Contact::mergeContactData));
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        for (String contactName : new String[]{"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+            contacts.computeIfAbsent(contactName, k -> new Contact(k));
+        }
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        for (String contactName : new String[]{"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+            contacts.computeIfPresent(contactName, (k,v) -> {v.addEmail("Fun Place"); return v;});
+        }
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        contacts.replaceAll((k,v) -> {
+            String newEmail = k.replaceAll(" ", "") + "@funplace.com";
+            v.replaceEmailIfExists("DDuck@funplace.com", newEmail);
+            return v;
+        });
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        Contact daisy  = new Contact("Daisy Duck", "daisyj@duck.com");
+
+        Contact replacedContact = contacts.replace("Daisy Duck", daisy);
+        System.out.println("daisy = " + daisy);
+        System.out.println("replacedContact = " + replacedContact);
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        Contact updatedDaisy = replacedContact.mergeContactData(daisy);
+        System.out.println("updatedDaisy = " + updatedDaisy);
+        boolean success = contacts.replace("Daisy Duck", replacedContact,  updatedDaisy);
+        if (success) {
+            System.out.println("updatedDaisy successfully");
+        }else {
+            System.out.printf("Did not match on both key: %s and value %s %n".formatted("Daisy Duck", daisy ));
+        }
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
+
+        System.out.println("-". repeat(100));
+        success = contacts.remove("Daisy Duck", daisy);
+        if (success) {
+            System.out.println("Successfully removed element");
+        }else {
+            System.out.printf("Did not match on both key: %s and value %s %n".formatted("Daisy Duck", daisy ));
+        }
+        contacts.forEach((k,v)-> System.out.println("Key: " + k + " Value: " + v));
     }
 }
