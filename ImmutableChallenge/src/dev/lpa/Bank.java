b3a5e612721e511741a4e23f9701e29d692ae43d
AdamKatrenic <adamkatrenic@gmail.com>
2025-06-08 23:36:47 +0200

    Pridavam ImmutableChallnge

---
+++ ImmutableChallenge/src/dev/lpa/Bank.java
@@ -0,0 +1,52 @@
+package dev.lpa;
+
+import dev.bank.BankAccount;
+import dev.bank.BankCustomer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+
+    private final int routingNumber;
+    private long lastTransactionID = 1;
+    private final Map<String, BankCustomer> customers;
+
+    public Bank(int routingNumber) {
+        this.routingNumber = routingNumber;
+        customers = new HashMap<>();
+    }
+
+    public BankCustomer getCustomer(String id) {
+
+        BankCustomer customer = customers.get(id);
+        return customer;
+    }
+
+    public void addCustomer(String name, double checkingInitialDeposit, double savingsInitialDeposit) {
+
+        BankCustomer customer = new BankCustomer(name,checkingInitialDeposit,savingsInitialDeposit);
+        customers.put(customer.getCustomerId(), customer);
+    }
+
+    public boolean doTransaction(String id, BankAccount.AccountType accountType, double amount) {
+
+        BankCustomer customer = customers.get(id);
+        if(customer == null){
+            BankAccount account = customer.getAccount(accountType);
+            if(account != null){
+                if((account.getBalance() + amount) < 0){
+                    System.out.println("Insufficient funds.");
+                }else {
+                    account.commitTransaction(routingNumber,lastTransactionID++,id,amount);
+                    return true;
+                }
+            }
+        }else {
+            System.out.println("Invalid customer ID");
+        }
+        return false;
+    }
+
+
+}
---
+++ ImmutableChallenge/src/dev/lpa/Main.java
@@ -0,0 +1,35 @@
+package dev.lpa;
+
+import dev.bank.BankAccount;
+import dev.bank.BankCustomer;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        BankAccount account = new BankAccount(BankAccount.AccountType.CHECKING,500);
+//        System.out.println(account);
+
+//        BankCustomer joe = new BankCustomer("Joe" , 500.00, 10000.00);
+//        System.out.println(joe);
+
+        Bank bank = new Bank(3214567);
+        bank.addCustomer("Joe",500.00,10000.00);
+
+
+
+        BankCustomer joe = bank.getCustomer("000000010000000");
+        System.out.println(joe);
+
+        if (bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING,35)){
+            System.out.println(joe);
+        }
+
+        if (bank.doTransaction(joe.getCustomerId(), BankAccount.AccountType.CHECKING,-535)){
+            System.out.println(joe);
+        }
+
+    }
+}
