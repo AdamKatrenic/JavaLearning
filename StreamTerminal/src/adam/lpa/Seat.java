58126d6e9241692c907d8dc0364bd9b48053741c
AdamKatrenic <adamkatrenic@gmail.com>
2025-06-16 22:54:03 +0200

    Pridanie StreamTerminal

---
+++ StreamTerminal/src/adam/lpa/Seat.java
@@ -0,0 +1,13 @@
+package adam.lpa;
+
+import java.util.Random;
+
+public record Seat(char rowMaker, int seatNumber, boolean isReserved) {
+
+    public Seat(char rowMaker, int seatNumber) {
+//        this(rowMaker, seatNumber, new Random().nextBoolean());
+
+        this(rowMaker, seatNumber, false);
+    }
+
+}
