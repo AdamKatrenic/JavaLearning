831f528ee47dd6662c5a488eb9504a797098f237
AdamKatrenic <adamkatrenic@gmail.com>
2025-07-08 19:05:05 +0200

    Pridavam WritingFiles

---
+++ WritingFiles/src/adam/lpa/student/Course.java
@@ -0,0 +1,24 @@
+package adam.lpa.student;
+
+import java.util.StringJoiner;
+
+public record Course(String courseCode, String title) {
+
+    public int getLectureCount() {
+        return 15;
+    }
+
+    @Override
+    public String toString() {
+        return "%s %s".formatted(courseCode, title);
+    }
+
+    public String toJSON() {
+        return new StringJoiner(", ", "{", "}")
+                .add("\"courseCode\":\"" + courseCode + "\"")
+                .add("\"title\":\"" + title + "\"")
+                .toString();
+    }
+}
+
+
